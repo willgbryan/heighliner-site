67b70970d5c3c0bef95107d55a6696c06127f5c3
willgbryan <will.bryan421@gmail.com>
2024-07-16 11:15:35 -0500

    clean up cards

--- src/components/cult/infinite-moving-cards.tsx
+++ src/components/cult/infinite-moving-cards.tsx
@@ -87,10 +87,10 @@ export const InfiniteMovingCards = ({
       >
         {items.map((item, idx) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-t-0 border-b-0 flex-shrink-0 border-stone-700 px-8 py-6 md:w-[450px]"
+            className="w-[350px] max-w-full relative rounded-xl border flex-shrink-0 border-stone-700 px-8 py-6 md:w-[450px]"
             style={{
               background:
-                "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
+                "linear-gradient(180deg, var(--stone-800), var(--stone-900)",
             }}
             key={item.name}
           >
