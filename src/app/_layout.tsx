0fffb29bf8fb440220220ebe6715aeabb433bb54
willgbryan <will.bryan421@gmail.com>
2024-07-17 12:40:41 -0500

    tweakin

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -72,7 +72,7 @@ export default function LandingPageLayout({
               exit={{ opacity: 0, y: 120 }}
               transition={{ duration: 1.6, delay: 0.9, type: "spring" }}
             >
-              <div className="flex justify-center items-center flex-col px-4">
+              <div className="flex justify-center items-center flex-col px-4 mt-4">
                   <p className="text-neutral-500 dark:text-neutral-400 text-xl md:text-3xl max-w-3xl mx-auto mb-10">
                     <LinkPreview url="https://themagi.systems" className="font-normal">
                       Magi
