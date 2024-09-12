104e735b7ea540bc3365fbd29a785ba3c79537dd
willgbryan <107731540+willgbryan@users.noreply.github.com>
2024-09-11 22:01:09 -0500

    code cleaning

--- src/components/cult/gui/statsGUI.js
+++
@@ -1,9 +0,0 @@
-import Stats from 'stats.js';
-
-export function createStatsGUI() {
-  const stats = new Stats()
-  stats.dom.style.position = 'absolute'
-  stats.dom.style.left = '0px'
-  stats.dom.style.top = '0px'
-  return stats;
-}
