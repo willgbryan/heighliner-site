929404a807b7b15e5856659bc85c4c3e66d284f1
willgbryan <107731540+willgbryan@users.noreply.github.com>
2024-10-02 22:20:16 -0500

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css"
 import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import type { Metadata } from 'next'
 import { Toaster } from "sonner"
 
 import { cn } from "@/lib/utils"
@@ -8,7 +8,45 @@ import { Analytics } from "@/components/analytics"
 import { OrganicButton } from "@/components/cult/organic-button"
 import TextAnimate from "@/components/cult/text-animate"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
+import { siteConfig } from "@/config/site"
 
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: ['Heighliner', 'Legal', 'Research', 'Artifacts'],
+  authors: [{ name: 'Will' }],
+  creator: 'Will',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://heighliner.ai',
+    title: 'Heighliner',
+    description: 'Legal research modernized',
+    siteName: 'Heighliner',
+    images: [{
+      url: 'https://s.yimg.com/ny/api/res/1.2/6YLkalF05.eeHi15u_0_.w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTMyMA--/https://o.aolcdn.com/hss/storage/midas/29cdfcf2bb2af26b103c432d02cbe5e2/205180329/TN-JPL1978-300dpi-ed2.jpg',
+      width: 640,
+      height: 320,
+      alt: 'Heighliner preview image',
+    }],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Heighliner',
+    description: 'Legal research modernized',
+    images: ['https://s.yimg.com/ny/api/res/1.2/6YLkalF05.eeHi15u_0_.w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTMyMA--/https://o.aolcdn.com/hss/storage/midas/29cdfcf2bb2af26b103c432d02cbe5e2/205180329/TN-JPL1978-300dpi-ed2.jpg'],
+  },
+  icons: {
+    icon: '/icon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: '/site.webmanifest',
+  metadataBase: new URL('https://heighliner.ai'),
+}
 
 interface RootLayoutProps {
   children: React.ReactNode
---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: '',
+    url: '',
+    ogImage: '',
+    description: '',
+    links: {
+      twitter: '',
+      github: '',
+    },
+  }
+  
+  export type SiteConfig = typeof siteConfig
+  
